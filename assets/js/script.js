a40ec308435c886e77c609089b8dd78b53d00530
Razzoy <1081892@ucn.dk>
2024-02-13 13:45:54 +0100

    Finished model for productData

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,39 @@
+
 getProductsData();
 
-//create function for receiving the database data
+//Create function for receiving the database data.
 function getProductsData(){
-    //fetching the database
+    //Fetching the database.
     fetch('https://dummyjson.com/products')
-    
+
+    //Convert the response to JSON format.
     .then(response => response.json())
+
+    //Pass JSON to the productsDataReceived function.
     .then(json => productsDataReceived(json));
 }
 
+//Create function for picking three products for main page.
 function productsDataReceived(productData){
 
+    //Create a variable that becomes equal to all 30 products in the database.
     let products = productData.products;
 
+    //Create a variable that becomes an empty array for adding the three products.
     let productList = [];
 
+    //Push three randomly chosen products to the empty array out from the 30 available products.
     productList.push(products[7], products[23], products[15]);
 
+    //console log the result.
     console.log(productList);
 
+    getProductPrices(productList);
+
+}
+
+function getProductPrices(productPrices){
+    
 }
 
 getCatgoryData();
